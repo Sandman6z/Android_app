8add3d4049587f829fb874ef2c10394710187ef6
Sandman006 <zbn0238@163.com>
2021-05-04 10:14:33 +0800

    app code

---
+++ MyApplication/app/src/main/java/com/pig/zbn02/myapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.pig.zbn02.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
